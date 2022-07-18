96250e32a5bf6bb686aedab901a96d149534ec45
Jirka1993 <kleric13@seznam.cz>
2022-07-18 19:59:38 +0200

    CommentForm add new comment logic

--- src/components/commentText/CommentForm.jsx
+++ src/components/commentText/CommentForm.jsx
@@ -2,23 +2,18 @@ import React, { useState } from "react";
 
 import "./comment.style.css";
 
-// const COMMENTS = [
-//   { id: 1, date: "2022-07-17T22:41:00", comment: "Ahoj", name: "Lojza" },
-//   { id: 2, date: "2022-07-17T22:42:00", comment: "Nazdar", name: "Peter" },
-// ];
+const COMMENTS = [];
 
 const CommentForm = (props) => {
-  //   const [comments, setComments] = useState(COMMENTS);
+  const [comments, setComments] = useState(COMMENTS);
   const [comment, setComment] = useState("");
   const [name, setName] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // console.log(`TEXT: ${comment} ${name}  `);
-
     if (comment && name) {
-      //   setComments((prevComments) => [{ id: Math.floor(Math.random() * 1000), date: new Date().toLocaleString(), comment, name }, ...prevComments]);
+      setComments((prevComments) => [{ id: Math.floor(Math.random() * 1000), date: new Date().toLocaleString(), comment, name }, ...prevComments]);
       setComment("");
       setName("");
     }
@@ -39,13 +34,15 @@ const CommentForm = (props) => {
         <button>add comment </button>
       </form>
 
-      {/* {comments.map((a) => (
-      <div className="article--c">
-        <h4>{a.date}</h4>
-        <h3 className="author">{a.name}</h3>
-        <p>{a.comment}</p>
-      </div>
-      ))} */}
+      {comments.map((a) => (
+        <div className="commentStyle">
+          <div className="commentStyle--top">
+            <h3 className="author">{a.name}</h3>
+            <h4>{a.date}</h4>
+          </div>
+          <p>{a.comment}</p>
+        </div>
+      ))}
     </div>
   );
 };
